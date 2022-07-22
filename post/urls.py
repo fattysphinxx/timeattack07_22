31bdc6b5bb979be797288dfe56dd583d65e00045
fattysphinxx <rlawjdgn159@gmail.com>
2022-07-22 17:10:15 +0900

    timeattack_0722

---
+++ post/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import (
+    SkillView, JobView, ApplyView
+)
+
+urlpatterns = [
+    path('', SkillView.as_view()),
+
+    path('job', JobView.as_view()),
+    path('apply', ApplyView.as_view()),
+    path('apply/<str:status_id', ApplyView.as_view()),
+
+]
